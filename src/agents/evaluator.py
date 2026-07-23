160dc458997452e525146965548eca96898bf9e4
azoozaws <azoozaws24@gmail.com>
2026-07-23 23:15:54 +0300

    fix: update model imports and bindings for consistency across evaluator, executor, and summarizer agents

--- src/agents/evaluator.py
+++ src/agents/evaluator.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from src.agents.agent_state import AgentState
-from src.models import gemini_3_5_flash_model, gemini_3_1_flash_model
+from src.models import gemini_3_6_flash_model, gemini_3_5_flash_model
 from src.prompts.prompts import evaluater_prompt
 from src.utils.get_yesterday_context import get_yesterday_context
 
@@ -18,8 +18,8 @@ from  src.tools.search_tavily import search_engine
 async def evaluate_step(state:AgentState):
     tools = [get_notion_monthly_goals, get_notion_weekly_objectives, get_tasks_by_date_range, search_notion_tasks, update_months_ids, update_month_sections_ids, search_engine]
 
-    primary_model_with_tools = gemini_3_5_flash_model.bind_tools(tools)
-    fallback_model_with_tools = gemini_3_1_flash_model.bind_tools(tools)
+    primary_model_with_tools = gemini_3_6_flash_model.bind_tools(tools)
+    fallback_model_with_tools = gemini_3_5_flash_model.bind_tools(tools)
     evaluater_model = primary_model_with_tools.with_fallbacks([fallback_model_with_tools])
 
     """
--- src/agents/executor.py
+++ src/agents/executor.py
@@ -4,7 +4,7 @@ from langchain_core.messages import HumanMessage
 
 from src.agents.agent_state import AgentState
 from langchain_core.output_parsers import StrOutputParser
-from src.models import gemini_3_5_flash_model, gemini_3_1_flash_model
+from src.models import gemini_3_6_flash_model, gemini_3_5_flash_model
 from src.prompts.prompts import executor_prompt
 
 from src.tools.notion_tools import(
@@ -27,8 +27,8 @@ async def execute_step(state:AgentState):
     tools = [get_notion_monthly_goals, get_notion_weekly_objectives, get_tasks_by_date_range, search_notion_tasks, update_months_ids, update_month_sections_ids, create_one_notion_task, create_multiple_notion_tasks, update_notion_task, delete_notion_task, search_engine]
 
     
-    primary_model_with_tools = gemini_3_5_flash_model.bind_tools(tools)
-    fallback_model_with_tools = gemini_3_1_flash_model.bind_tools(tools)
+    primary_model_with_tools = gemini_3_6_flash_model.bind_tools(tools)
+    fallback_model_with_tools = gemini_3_5_flash_model.bind_tools(tools)
     executor_model = primary_model_with_tools.with_fallbacks([fallback_model_with_tools])
 
     """
